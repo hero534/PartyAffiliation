2d5d6aa0419057ab8522c69d2ed712b228fc7f3b
hgold <goldsmhr@mail.uc.edu>
2025-02-10 11:57:49 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String RDIMove = "";
+            System.out.println("What is your party affiliation?");
+            RDIMove = in.next();
+                if (RDIMove == "R") {
+                    System.out.println("You get a Republican Elephant!");
+                } else if (RDIMove == "D") {
+                    System.out.println("You get a Democratic Donkey!");
+                } else if (RDIMove == "I") {
+                    System.out.println("You get a Independent Man!");
+                } else "You"
+    }
+}
